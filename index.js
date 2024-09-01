8940072ff6021f22f87f161c08208864be1f33c4
Bhavya Prajapati <126806988+bhavyaGP@users.noreply.github.com>
2024-09-01 21:29:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log("This is from Bhavya DellG15 -0");
 console.log("This is from Bhavya DellVostro -0");
 console.log("This is from Bhavya DellG15 -1");
 console.log("This is from Bhavya DellVostro -1")
+console.log("This is from Bhavya DellG15 -2");
